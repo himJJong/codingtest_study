2f20e513dc8cc99cc0a55467e542cbcb71b7c7fc
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2024-08-16 18:59:14 +0900

    codeTree_사각채우기2

---
+++ 24_08_07/codeTree_사각채우기2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class codeTree_사삭채우기2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        if(N <= 2){
+            if(N == 1){
+                System.out.println(1);
+
+            }
+            else if(N==2){
+                System.out.println(3);
+            }
+            System.exit(0);
+        }
+
+        int[] dp = new int[N+1];
+        dp[1] = 1;
+        dp[2] = 3;
+
+
+        for(int i=3; i<=N; i++){
+            if(i % 2 == 1){
+                dp[i] = (dp[i-1] * 2 - 1) % 10007;
+            }
+            else{
+                dp[i] = (dp[i-1] * 2 + 1) % 10007;
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
