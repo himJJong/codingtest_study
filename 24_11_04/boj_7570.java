8138333323fa0a8af2077b97732e1e7d97379279
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2024-11-09 16:23:15 +0900

    트리의 독립집합

--- 24_11_04/boj_7570.java
+++ 24_11_04/boj_7570.java
@@ -1,15 +1,19 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class boj_7570 {
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] data = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+    public static void main(String[] args) throws IOException {
 
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] dp = new int[n+1];
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++) {
+            int k = scan.nextInt();
+            dp[k] = dp[k-1]+1;
+            max = Math.max(dp[k], max);
+        }
 
+        System.out.println(n-max);
     }
 }
